4c19f5ea312576d95f806765fc3ed7d3ad0337a9
jujinyoung <jujinyoung1838@naver.com>
2021-09-07 00:03:12 +0900

    미로탐색
    bfs알고리즘

--- bakjjun_codingTest/2178.py
+++ bakjjun_codingTest/2178.py
@@ -1,11 +1,11 @@
 import sys
 N, M =map(int,sys.stdin.readline().split())
 s = []
-queue = []
 dx = [1,-1,0,0]
 dy = [0,0,-1,1]
 for i in range(N):
     s.append(list(sys.stdin.readline()))
+s[0][0] = 1
 queue = [[0,0]]
 while queue:
     a,b=queue[0][0],queue[0][1]
@@ -16,3 +16,4 @@ while queue:
         if 0<=x<N and 0<=y<M and s[x][y] == "1":
             queue.append([x,y])
             s[x][y] = s[a][b] + 1
+print(s[N-1][M-1])
