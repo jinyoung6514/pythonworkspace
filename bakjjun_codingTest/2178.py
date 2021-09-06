828b0e1792f034f0c36fc82f85c2566696c2e238
jujinyoung <jujinyoung1838@naver.com>
2021-09-06 23:44:55 +0900

    미로탐색
    bfs알고리즘

---
+++ bakjjun_codingTest/2178.py
@@ -0,0 +1,18 @@
+import sys
+N, M =map(int,sys.stdin.readline().split())
+s = []
+queue = []
+dx = [1,-1,0,0]
+dy = [0,0,-1,1]
+for i in range(N):
+    s.append(list(sys.stdin.readline()))
+queue = [[0,0]]
+while queue:
+    a,b=queue[0][0],queue[0][1]
+    del queue[0]
+    for i in range(4):
+        x = a + dx[i]
+        y = b + dy[i]
+        if 0<=x<N and 0<=y<M and s[x][y] == "1":
+            queue.append([x,y])
+            s[x][y] = s[a][b] + 1
