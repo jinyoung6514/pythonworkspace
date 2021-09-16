272a1feac97159e67802c5e59992c4a91e092874
jujinyoung <jujinyoung1838@naver.com>
2021-09-16 23:50:26 +0900

    dfs, bfs 알고리즘
    dfs, bfs

--- bakjjun_codingTest/1260.py
+++ bakjjun_codingTest/1260.py
@@ -1,23 +1,34 @@
 import sys
 from collections import deque
-N,M,V = map(int,sys.stdin.readline().split())
+N, M, V = map(int,sys.stdin.readline().split())
+s = [[0]*N for i in range(N)]
+dfs_visit = [0 for i in range(N)]
+bfs_visit = [0 for i in range(N)]
+q = deque()
 
-visit = [0] * (N+1)
-graph = [0][0]
-def bfs(V):
-    q = deque()
-    q.append(V)
-    visit[V] = 1
+for i in range(M):
+    a,b = map(int,sys.stdin.readline().split())
+    s[a-1][b-1] = 1
+    s[b-1][a-1] = 1
+
+def dfs(v):
+    dfs_visit[v] = 1
+    print(v+1,end=" ")
+    for i in range(N):
+        if s[v][i] == 1 and dfs_visit[i] == 0:
+            dfs(i)
+
+def bfs(v):
+    q.append(v)
+    bfs_visit[v] = 1
     while q:
-        V = q.popleft()
-        print(V, end=" ")
-        for i in range(1,N+1):
-            if visit[i] == 0 and graph[V][i] == 1:
-                q.append[i]
-                visit[i] = 1
+        x = q.popleft()
+        print(x+1, end=" ")
+        for i in range(N):
+            if s[x][i] == 1 and  bfs_visit[i] == 0:
+                q.append(i)
+                bfs_visit[i] = 1
 
-def dfs():
-    pass
-
-for _ in range(M):
-    pass
+dfs(V-1)
+print()
+bfs(V-1)
