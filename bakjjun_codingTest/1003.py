99cd486b7ffd363ead538edaae189817ece74cbd
jujinyoung <jujinyoung1838@naver.com>
2021-09-28 23:11:46 +0900

    피보나치 함수
    동적 프로그래밍

---
+++ bakjjun_codingTest/1003.py
@@ -0,0 +1,10 @@
+import sys
+t = int(sys.stdin.readline())
+result = [[1,0],[0,1]]
+for i in range(2,41):
+    result.append([result[i-2][0] + result[i-1][0],result[i-2][1]+result[i-1][1]])
+
+
+for _ in range(t):
+    n = int(sys.stdin.readline())
+    print(result[n][0],result[n][1])
