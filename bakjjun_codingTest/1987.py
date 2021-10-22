674343db92e68dc471a085abcf33c520192858a9
jujinyoung <jujinyoung1838@naver.com>
2021-10-23 07:53:04 +0900

    Update 1987.py

--- bakjjun_codingTest/1987.py
+++ bakjjun_codingTest/1987.py
@@ -22,7 +22,6 @@
 
 import sys
 
-# 좌, 하, 우, 상
 dx = [-1, 0, 1, 0]
 dy = [0, -1, 0, 1]
 
@@ -34,12 +33,10 @@ def BFS(x, y):
     while q:
         x, y, ans = q.pop()
 
-        # 좌우상하 갈 수 있는지 살펴본다
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            # index 벗어나지 않는지 체크하고, 새로운 칸이 중복되는 알파벳인지 체크한다
             if ((0 <= nx < R) and (0 <= ny < C)) and (board[nx][ny] not in ans):
                 q.add((nx,ny,ans + board[nx][ny]))
                 answer = max(answer, len(ans)+1)
