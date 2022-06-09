84982068130deea73fe924029c33a60e125d5a7a
jujinyoung <jujinyoung1838@naver.com>
2022-06-09 17:23:46 +0900

    N과 M(4)
    DFS알고리즘 자바는 제출할때 클래스명을 Main으로 바꾸고 제출해야함

---
+++ bakjjun_codingTest/_15652.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _15652{
+    static int answer[];
+    static int N,M;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception{
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer stk = new StringTokenizer(br.readLine()," ");
+   
+       N = Integer.parseInt(stk.nextToken());
+       M = Integer.parseInt(stk.nextToken());
+
+       answer = new int[M];
+        
+       Dfs(1,0);
+
+       System.out.print(sb);
+    }
+
+    public static void Dfs(int start,int depth){
+        if(depth == M){
+            for(int val : answer){
+                sb.append(val).append(' ');
+            }
+            sb.append("\n");
+            return;
+       }
+       
+       for(int i = start; i <= N; i++){
+            answer[depth] = i;
+            Dfs(i, depth+1);
+        }
+    }
+}
