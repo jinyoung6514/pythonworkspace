541f037f297f17562e3f0406813905ab86c32106
jujinyoung <jujinyoung1838@naver.com>
2022-08-06 17:12:39 +0900

    문자열 폭발
    스택활용해서 폭탄길이만큼 뒤에서 비교

---
+++ bakjjun_codingTest/_9935.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class _9935 { 
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    String input = br.readLine();
+    String bomb = br.readLine();
+    int bombSize = bomb.length();
+    boolean check = true;
+    Stack<Character> stack = new Stack<>();
+
+    for(int i=0; i<input.length(); i++){
+      stack.push(input.charAt(i));
+
+      if(stack.size() >= bombSize){
+        check = true;
+        for(int j=0; j<bombSize; j++){
+          //폭탄길이만큼 뒤에서 문자 같은지 체크
+          if(stack.get(stack.size()-bombSize+j) != bomb.charAt(j)){
+            check = false;
+            break;
+          }
+        }
+        if(check){
+          for(int j=0; j<bombSize; j++){
+            stack.pop();
+          }
+        }
+      }
+    }
+
+    if(stack.isEmpty()){
+      System.out.println("FRULA");
+    }else{
+      for(int i=0; i<stack.size(); i++){
+        sb.append(stack.get(i));
+      }
+      System.out.println(sb);
+    }
+  }
+}
